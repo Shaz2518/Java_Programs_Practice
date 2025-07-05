1ddece9137d902e16128108b4288062556a80b6b
Shaziya Shaikh <156684996+Shaz2518@users.noreply.github.com>
2025-07-05 22:20:43 +0530

    Add files via upload

---
+++ Loops_Practice.java
@@ -0,0 +1,17 @@
+
+public class Loops_Practice {
+
+	public static void main(String[] args) {
+		for(int i =10; i<=16; i++)
+		{
+			//System.out.println("Hello");
+		}
+		
+		//Print numbers from 1-10
+		for (int i=1; i<=100; i++)
+		{
+			System.out.println(i);
+		}
+	}
+
+}
